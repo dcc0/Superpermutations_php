eff59179b21b82b59202777c578ddf575c8d7a74
dcc0 <ivangavr777@gmail.com>
2018-12-09 11:55:38 +0300

    Update superpermutations.php

--- superpermutations.php
+++ superpermutations.php
@@ -8,48 +8,43 @@ $seq="12345";
 //Счетчик
 $couter=0;
     for ($i=1; $i != $fact+1; $i++) {
-
-			print $perm . "\n";
-			$seq.=$perm[$n-1];
-
-			$mm = $i;
-			$m=$n;
-
-			while ($m > 0) {
-			//Проверка  на делимость
-			if ($mm%$m==0)  {
-				//Увеличим счетчик
-				$couter++;
-				$mm /= $m--;
-				print ("-----\n");
-			}
-			else
-			{
-				//Счетчик равен 0, допишем хвостовую часть и
-				//Обнулим счетчик
-			if ($couter != 0) {
-			$tail_seq=strrev(substr($perm, 0, $n-$m+1));
-			$seq.=$tail_seq;
-			$couter=0;
-			}
-			//Разберем перестановку на две части, обернем первую,
-			//соберем обратно, вернемся во внешний цикл
-			$tail= strrev(substr($perm, 0, $n-$m+1));
-			$head = substr($perm,  $n-$m+1);
-			$perm=$head.$tail;
-			continue 2;
-				}
-
-			}
-
+            print $perm . "\n";
+            $seq.=$perm[$n-1];
+            $mm = $i;
+            $m=$n;
+            while ($m > 0) {
+            //Проверка  на делимость
+            if ($mm%$m==0)  {
+                //Увеличим счетчик
+                $couter++;
+                $mm /= $m--;
+                print ("-----\n");
+            }
+            else
+            {
+                //Счетчик равен 0, допишем хвостовую часть и
+                //Обнулим счетчик
+            if ($couter != 0) {
+            $tail_seq=strrev(substr($perm, 0, $n-$m+1));
+            $seq.=$tail_seq;
+            $couter=0;
+            }
+            //Разберем перестановку на две части, обернем первую,
+            //соберем обратно, вернемся во внешний цикл
+            $tail= strrev(substr($perm, 0, $n-$m+1));
+            $head = substr($perm,  $n-$m+1);
+            $perm=$head.$tail;
+            continue 2;
+                }
+            }
 }
 //Вывод. Уберем повторы
 $sum=0;
 for ($k=0; $k!= strlen($seq)-1; $k++) {
 if ($seq[$k]!=$seq[$k+1]) {
-	$sum++;
+    $sum++;
 print  ($seq[$k]);
-	}
+    }
 }
 print "\n Сумма:" . ($sum);
 ?>
